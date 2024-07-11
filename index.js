23c310d5935e74687b72aff516fe13e20702bc7f
Hosni Bounechada <hosni.bounechada@lastingdynamics.com>
2024-07-11 12:45:03 +0100

    new commit in the same PR fix-234

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT | 3000;
 
 app.get("/", (req, res) => {
   console.log("fixed in fix-234");
+  console.log("new commit int the same PR");
   res.send("GET request is working");
 });
 
