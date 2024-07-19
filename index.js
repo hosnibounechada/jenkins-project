06be396326e99362a0ce5ba4e2f43149f8a83cb6
Hosni Bounechada <hosni.bounechada@lastingdynamics.com>
2024-07-19 11:04:43 +0100

    develop branch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const PORT = process.env.PORT | 3000;
 
 app.get("/", (req, res) => {
-  console.log("Feature f-123");
+  console.log("develop branch");
   res.send("GET request is working");
 });
 
