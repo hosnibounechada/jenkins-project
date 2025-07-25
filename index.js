7bc3746bd2abc888a1849b03f04b2b66b8b1f063
Hosni Bounechada <hosni.bounechada@lastingdynamics.com>
2025-07-25 16:22:19 +0100

    Test: jenkins webhook debug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const PORT = process.env.PORT | 3000;
 
 app.get("/", (req, res) => {
-  console.log("main branch update");
+  console.log("main branch update again");
   res.send("GET request is working");
 });
 
